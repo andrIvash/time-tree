353f411d6053daab6880c0e9d86fcc6d1e67bcdd
andrivash <andr.ivas12@gmail.com>
2017-01-04 20:38:11 +0200

    add base styles

--- src/app/addblock/addblock.component.ts
+++ src/app/addblock/addblock.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ViewEncapsulation, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-addblock',
   templateUrl: './addblock.component.html',
-  styleUrls: ['./addblock.component.scss']
+  styleUrls: ['./addblock.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AddblockComponent implements OnInit {
 
